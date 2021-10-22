8d71e00cc3d4d8b28887ab91922f38d248002b9f
thesstefan <kalinpopa@outlook.com>
2021-10-22 13:26:36 +0300

    backup

--- Bakery/test.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by Andrei on 3/20/2021.
-//
-
-#include "test.h"
-
-int _CrtDumpMemoryLeaks() {
-    return 0;
-}
--- Bakery/test.h
+++
@@ -1,7 +0,0 @@
-//
-// Created by Andrei on 3/20/2021.
-//
-
-#pragma once
-
-int _CrtDumpMemoryLeaks();
